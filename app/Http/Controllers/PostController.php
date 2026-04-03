d6e5a63cac7324b86abd532055cb7db5fc99b32a
Gun Gun <gungun@gmail.com>
2026-04-03 09:25:02 +0700

    init repo

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str; // add this line of code
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $posts = Post::latest()->paginate(10);
+        return view('posts.index', compact('posts'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('posts.create');
+    }
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->merge([
+            'slug' => Str::slug($request->title),
+        ]);
+
+        $validatedData = $request->validate([
+            'title' => 'required|max:255',
+            'slug' => 'required|unique:posts,slug|max:255',
+            'content' => 'required',
+            'status' => 'required|in:draft,publish',
+        ]);
+
+        Post::create($validatedData);
+
+        return redirect()->route('posts.index')->with('success', 'Post created successfully.');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Post $post)
+    {
+        return view('posts.show', compact('post'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Post $post)
+    {
+        return view('posts.edit', compact('post'));
+    }
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Post $post)
+    {
+        $request->merge([
+            'slug' => Str::slug($request->title),
+        ]);
+
+        $validatedData = $request->validate([
+            'title' => 'required|max:255',
+            'slug' => 'required|unique:posts,slug,' . $post->id . '|max:255',
+            'content' => 'required',
+            'status' => 'required|in:draft,publish',
+        ]);
+
+        $post->update($validatedData);
+
+        return redirect()->route('posts.index')->with('success', 'Post updated successfully.');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Post $post)
+    {
+        $post->delete();
+
+        return redirect()->route('posts.index')->with('success', 'Post deleted successfully.');
+    }
+}
